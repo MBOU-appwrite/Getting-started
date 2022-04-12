3d2766981c7a7d5f00ccb2401e4aaa2b995f8e8e
DarrenSanders <332030@student.mboutrecht.nl>
2022-04-12 18:28:44 +0200

    moved the app.js and index.html to different locations

---
+++ app/app.js
@@ -0,0 +1,7 @@
+const appWrite = new Appwrite();
+
+appWrite
+    .setEndpoint('http://localhost/v1') // Your Appwrite Endpoint
+    .setProject('624d7fd5ddadc8fecda0') // Your project ID
+
+console.log(appWrite)
